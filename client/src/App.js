1118a43fbd1c3f7618f46fad7692d63f7088adc6
shastakouski <36038361+shastakouski@users.noreply.github.com>
2021-01-18 18:54:56 +0100

    Configure https ingress-service

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator vercion 2 AAA BBB CCC
+            Fib calculator vercion 2 AAA BBB CCC DDD
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
